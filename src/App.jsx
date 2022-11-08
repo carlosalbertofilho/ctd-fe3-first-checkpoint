6fbe6bc2e7cdf5d3dd366034b0fb30482f4fca5a
carlosalbertofilho <carlosalberto_filho@outlook.com>
2022-11-07 22:47:45 -0300

    primeira parte

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,55 @@
 
 // Aqui você irá escrever as suas funções de Validação, para verificar se o Formulário foi preenchido corretamente
 
+import { useState } from "react"
+import { Card } from "./Card"
+
 function App() {
   // Aqui você irá criar os Estados para manipular os Inputs
 
+  const [nomeCor, setNomeCor] = useState('')
+  const [codigoCor, setCodigoCor] = useState('')
+  const [error, setError] = useState(false)
+  const [listaCores, setListaCores] = useState([])
+
+  const cadastraCor = (event) => {
+    event.preventDefault();
+    const novaCor = {
+      nome: nomeCor
+      , codigo: codigoCor
+    }
+
+    setListaCores(novaCor, ...listaCores)
+
+  }
 
   return (
     <div className="App">
-     <h1>Carga de estudiantes</h1>
-     <form>
-      {/* Comece a desenvolver o seu Código por aqui :) */}
-     </form>
+     <main>
+      <h1>ADICIONAR NOVA COR</h1>
+      <form onSubmit={event => cadastraCor(event)}>
+        <label htmlFor="nomeCor">Nome:</label>
+        <input type="text" name="nomeCor" value={nomeCor} onChange={event => setNomeCor(event.target.value)} />
+        <label htmlFor="inputCor">Cor:</label>
+        <input type="color" name="inputCor" value={codigoCor} onChange={event => setCodigoCor(event.target.value)}/>
+        <button type="submit">Adicionar</button>
+      </form>
+     </main>
+     <section>
+      <h1>CORES FAVORITAS</h1>
+        {
+          listaCores.map(
+            cor => {
+              console.log(cor)
+              return(
+                <Card 
+                  corData={cor}
+                />
+              )
+            }
+          )
+        }
+     </section>
     </div>
   )
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,10 @@
 //Este componente deverá receber dados por Props e mostrar as Informações em Tela
 
-export function Card () {
+export function Card (props) {
   return (
     <div>
-
+      <h1>{props.nome}</h1>
+      <span>{props.codigo}</span>
     </div>
   )
 }
